067a219ad74f56773c913b820d901600c28f2226
Jose <joseossorio99@hotmail.com>
2021-02-08 18:33:10 -0500

    finish js functionality

---
+++ index.js
@@ -0,0 +1,61 @@
+
+const adminModal = document.getElementById("adminModal");
+const projectModal = document.getElementById("projectModal");
+const accountModal = document.getElementById("accountModal");
+const changeModal = document.getElementById("changeModal");
+const leadModal = document.getElementById("leadModal");
+const creativeModal = document.getElementById("creativeModal");
+const organizedModal = document.getElementById("organizedModal");
+const seriousModal = document.getElementById("seriousModal");
+
+
+document.getElementById("adminSpan").onclick = () => {
+        adminModal.style.display = "none";
+    };
+document.getElementById("projectSpan").onclick = () => {
+        projectModal.style.display = "none";
+    };
+document.getElementById("accountSpan").onclick = () => {
+        accountModal.style.display = "none";
+    };
+document.getElementById("changeSpan").onclick = () => {
+        changeModal.style.display = "none";
+    };
+document.getElementById("leadSpan").onclick = () => {
+        leadModal.style.display = "none";
+    };
+document.getElementById("creativeSpan").onclick = () => {
+        creativeModal.style.display = "none";
+    };
+document.getElementById("organizedSpan").onclick = () => {
+        organizedModal.style.display = "none";
+    };
+document.getElementById("seriousSpan").onclick = () => {
+        seriousModal.style.display = "none";
+    };
+
+
+document.getElementById("adminModalBtn").onclick = () => {
+        adminModal.style.display = "block";
+    };
+document.getElementById("projectModalBtn").onclick = () => {
+        projectModal.style.display = "block";
+    };
+document.getElementById("accountModalBtn").onclick = () => {
+        accountModal.style.display = "block";
+    };
+document.getElementById("changeModalBtn").onclick = () => {
+        changeModal.style.display = "block";
+    };
+document.getElementById("leaderModalBtn").onclick = () => {
+        leadModal.style.display = "block";
+    };
+document.getElementById("creativeModalBtn").onclick = () => {
+        creativeModal.style.display = "block";
+    };
+document.getElementById("organizedModalBtn").onclick = () => {
+        organizedModal.style.display = "block";
+    };
+document.getElementById("seriousModalBtn").onclick = () => {
+        seriousModal.style.display = "block";
+    };
